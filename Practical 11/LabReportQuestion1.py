7347a26fd22fa5a0d8e3e2acbdd4b6eefa5a87fa
lfcd2 <lfcd2@cam.ac.uk>
2023-03-04 22:07:59 +0000

    Practical 11 Annotated

--- Practical 11/LabReportQuestion1.py
+++ Practical 11/LabReportQuestion1.py
@@ -1,3 +1,10 @@
+"""
+Many of the comments were added by Louis De Neve for the sake of poor QES students who are stuck
+Finished, almost completely PEP-8 compliant code for most of the lab report is available on my GitHub:
+https://github.com/lfcd2/QES_Lent_Practicals
+"""
+
+
 import numpy as np
 from matplotlib import pyplot as plt
 
